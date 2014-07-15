6b9c7287633bdb5967a86d908f6db575b0705571
Konstantin Kosmatov <key@kosmatov.ru>
2014-07-15 15:52:16 +0400

    Premailer hook error when message w/out content_type

--- lib/premailer/rails/hook.rb
+++ lib/premailer/rails/hook.rb
@@ -42,6 +42,7 @@ class Premailer
       # Returns true if the message itself has a content type of text/html, thus
       # it does not contain other parts such as alternatives and attachments.
       def pure_html_message?
+        return unless message.content_type
         message.content_type.include?('text/html')
       end
 
